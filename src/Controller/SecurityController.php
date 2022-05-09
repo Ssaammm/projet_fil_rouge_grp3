92e7ec13b9afdbc8a9a190b3cda42cf7027178a4
Sam <smass59260@gmail.com>
2022-05-09 15:34:58 +0200

    redirection route login + affichage des cards

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     public function redirection() {
 
         if ($this->getUser()) {
-            return $this->redirectToRoute('app_task_affichage');
+            return $this->redirectToRoute('app_home');
         } else {
             return $this->redirectToRoute('app_login');
         }
@@ -28,7 +28,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //     return $this->redirectToRoute('app_home');
         // }
 
         // get the login error if there is one
